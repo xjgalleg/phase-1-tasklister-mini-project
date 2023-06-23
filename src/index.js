5b098c2b3432fe5931dcd928ae2a22b48403749e
Josue Gallegos <gallegosjosue6@gmail.com>
2023-06-23 08:36:07 -0500

    displays task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners();
 });
+
+function addingEventListeners() {
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit);
+}
+
+function handleFormSubmit(e) {
+    e.preventDefault()
+   const task = e.target[0].value
+
+   displayTask(task)
+}
+
+function displayTask(task) {
+  const taskUl = document.getElementById("tasks")
+  
+  const taskLi = document.createElement("li")
+
+  taskLi.textContent = task
+  taskUl.appendChild(taskLi)
+}
